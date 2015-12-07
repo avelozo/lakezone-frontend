d524ba96a8952c284a6145045efcb5f53ff12629
alevelozo <alessandrac.velozo@gmail.com>
2015-12-06 23:18:10 -0600

    Images being shown

--- js/ajax.js
+++ js/ajax.js
@@ -6,37 +6,9 @@ function searchProduct(){
   getJSON("product",product, function(data){
   	setLocalStorage("productList",data);
   	window.location="productList.html";
-  	// renderProductList();
-
   });
 }
 
-// function renderProductList(){
-// 	var total = "";
-// 	var productList = getLocalStorage("productList");
-// 	total += "<table class='table table-bordered table-hover'>";
-// 	total += "<thead>";
-// 	total += "<tr>";
-// 	total += "<th>Product</th>";
-// 	total += "<th>Price</th>";
-// 	total += "<th>Partner</th>";
-
-// 	total += "</tr>";
-// 	total += "</thead>";
-// 	total += "<tbody>";
-// 	for(var i=0; i<productList.length; i++){
-// 		total += "<tr>";
-// 		total += "<td>" + productList[i].name + "</td>";
-// 		total += "<td>" + productList[i].price + "</td>";
-// 		total += "<td>" + productList[i].productOwnerName + "</td>";
-
-// 		total += "</tr>";
-// 	}
-// 	total += "</tbody>";
-// 	total += "</table>";
-// 	$("#productList").html(total);
-
-// }
 
 function renderProductList(){
 	var total = "";
@@ -44,7 +16,7 @@ function renderProductList(){
 	for(var i=0; i<productList.length; i++){
 		total += "<div class='item  col-xs-4 col-lg-4'>";
 		total += "<div class='thumbnail'>";
-		total += "<img class='group list-group-image' src='http://placehold.it/400x250/000/fff' alt='' />";
+		total += "<img class='group list-group-image' src="+ productList[i].img + " alt='' />";
 		total += "<div class='caption'>";
 		total += "<h4 class='group inner list-group-item-heading'>"+productList[i].name+"</h4>";
 		total += "<p class='group inner list-group-item-text'>Partner: "+productList[i].productOwnerName+" </p>";
@@ -58,17 +30,9 @@ function renderProductList(){
 
 	}
 	
-
-
 	$("#products").html(total);
-
 }
-
-          
                         
-                        
-                           
-
 
 function getJSON(url,name, successCallback) {
 	$.ajax(
